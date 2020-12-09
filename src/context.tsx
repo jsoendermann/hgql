888af7fe4ec7e7f1636b71658d743f22c0580412
Jan Soendermann <jan.soendermann+git@gmail.com>
2020-12-09 19:54:50 +0000

    Extract react specific bits

--- src/context.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-import { SendRequestFunction } from './sendRequestFunction'
-
-export const HgqlContext = createContext<SendRequestFunction>(() => {
-  throw new Error('Invalid hgql client')
-})
-HgqlContext.displayName = 'HgqlContext'
